f2efc5b364c4540244cf0629c5808f35e3c25cbc
ProgecktProf <sergey.nikishin555123123@gmail.com>
2026-08-04 11:40:30 +0300

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,43 @@
+const winston = require('winston');
+const path = require('path');
+const fs = require('fs');
+const config = require('./config');
+
+const logDir = config.logging.dir;
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
+
+const logger = winston.createLogger({
+  level: config.logging.level,
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'learning-bot' },
+  transports: [
+    new winston.transports.File({
+      filename: path.join(logDir, 'error.log'),
+      level: 'error',
+      maxsize: 5242880,
+      maxFiles: 5,
+    }),
+    new winston.transports.File({
+      filename: path.join(logDir, 'app.log'),
+      maxsize: 5242880,
+      maxFiles: 10,
+    }),
+  ],
+});
+
+if (config.server.nodeEnv !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+  }));
+}
+
+module.exports = logger;
