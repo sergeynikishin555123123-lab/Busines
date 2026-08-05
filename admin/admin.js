012ee389a1a7eeb6da0c08182b90f5e1ba5831da
ProgecktProf <sergey.nikishin555123123@gmail.com>
2026-08-05 11:02:47 +0300

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs'); 
 const multer = require('multer');
 
 const database = require('../database');
