d1184e20de93406e6f5a50e93186e327b5bca6fe
ProgecktProf <sergey.nikishin555123123@gmail.com>
2026-08-11 18:29:40 +0300

    Update vk.js

--- platforms/vk.js
+++ platforms/vk.js
@@ -48,7 +48,7 @@ class VKAPI {
         this.token = config.vk.groupToken;
         this.apiVersion = config.vk.apiVersion || '5.131';
         this.baseUrl = 'https://api.vk.com/method';
-        this.confirmationToken = config.vk.confirmationToken || 'f8803dfc';
+        this.confirmationToken = config.vk.confirmationToken || '3bae5d25';
         this.groupId = config.vk.groupId;
 
         if (!this.token) {
