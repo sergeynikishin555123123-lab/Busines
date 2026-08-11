70691bdaaa2243b1ea72d146e18e5409ddd0485c
ProgecktProf <sergey.nikishin555123123@gmail.com>
2026-08-11 18:22:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ const config = {
   // ============================================================
   vk: {
     groupToken: process.env.VK_GROUP_TOKEN || '',
-   confirmationToken: process.env.VK_CONFIRMATION_TOKEN || 'f8803dfc',
+   confirmationToken: process.env.VK_CONFIRMATION_TOKEN || '3bee5d25',
     apiVersion: process.env.VK_API_VERSION || '5.131',
     secret: process.env.VK_SECRET || '',
     groupId: process.env.VK_GROUP_ID || '',
